be0a5a73b5aea79a2f254cb0a28c5a205da580e4
willsbor <willsbor@gmail.com>
2015-12-07 10:34:29 +0800

    Initial Commit

---
+++ AutoPullRequest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoPullRequest
+//
+//  Created by willsbor Kang on 2015/12/7.
+//  Copyright © 2015年 gogolook. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AutoPullRequest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoPullRequest
+//
+//  Created by willsbor Kang on 2015/12/7.
+//  Copyright © 2015年 gogolook. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
